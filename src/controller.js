425f2a71f1eceb7e39aec870b9be64b5f0894cff
JAnavarro77 <navarrojuanalberto@gmail.com>
2024-08-25 21:27:59 -0300

    metodo post funcionando

--- src/controller.js
+++ src/controller.js
@@ -6,6 +6,17 @@ class LibroController{
         const [result] = await pool.query('SELECT * FROM libros');
         res.json(result);
     }
+    async add(req, res){
+        const libro = req.body;
+       
+        const [result] = await pool.query(
+            `INSERT INTO libros (nombre, autor, categoria, año_publicacion, ISBN) VALUES (?, ?, ?, ?, ?)`, 
+            [libro.nombre, libro.autor, libro.categoria, libro.año_publicacion, libro.ISBN]
+        
+        );
+        res.json({"Libro Insertado": result.insertId})
+
+    }
 }
 
 export const libro = new LibroController();
